8f5ad9935225d76d4fafa4682ffb21937dcfe0d2
Titus Jung <titus@integratedproteomics.com>
2020-05-06 15:20:24 -0700

    updating version number

--- src/edu/scripps/pms/census/conf/Configuration.java
+++ src/edu/scripps/pms/census/conf/Configuration.java
@@ -121,7 +121,7 @@ public class Configuration {
     private boolean align=true;
     private boolean xmlConf=false;
 
-    private String version = "2.53.3";
+    private String version = "2.53.4";
     private String tmtOutlierLevel = "NO_OUTLIER";
 
 
